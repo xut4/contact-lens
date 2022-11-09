4212549fd1ff713428d618043bc9d411223f2ee3
tung <00957001@email.ntou.gmail.com>
2022-11-09 11:07:38 +0800

    Initial Commit

---
+++ hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw3
+//
+//  Created by User05 on 2022/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw3/hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  hw3App.swift
+//  hw3
+//
+//  Created by User05 on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
